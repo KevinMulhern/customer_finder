0e40d2c310bcef66bc4087722d5ea1737625cb44
KevinMulhen <kevinmulhern23@gmail.com>
2019-07-18 01:23:58 +0100

    Added Integration Specs
    For the scenarios described in the problem definition

---
+++ spec/integration/region_average_customer_value_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe 'Region Average Customer Value' do
+
+  let(:region) do
+    CustomerFinder::Region.new(
+      latitude: 51.450167,
+      longitude: -2.594678,
+      radius: 200
+    )
+  end
+
+  it 'returns average value of customers within 200km of Bristol' do
+    expect(region.average_customer_value).to eql(2467.73)
+  end
+end
---
+++ spec/integration/region_customers_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+RSpec.describe 'Region Customers' do
+
+  let(:region) do
+    CustomerFinder::Region.new(
+      latitude: 51.450167,
+      longitude: -2.594678,
+      radius: 100
+    )
+  end
+
+  it 'returns customers 100km from bristol' do
+    expect(JSON.parse(region.customers).size).to eql(20)
+  end
+
+  context 'when filtering by country' do
+    let(:region) do
+      CustomerFinder::Region.new(
+        latitude: 51.450167,
+        longitude: -2.594678,
+        radius: 100,
+        options: { filters: { country: 'england' } }
+      )
+    end
+
+    it 'returns customers 100km from bristol and from England' do
+      expect(JSON.parse(region.customers).size).to eql(16)
+    end
+  end
+end
